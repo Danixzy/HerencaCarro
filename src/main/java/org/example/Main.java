76ad54683d71b398f3aa84bf602d6e936e747607
Danix <xdanielmendoncax@gmail.com>
2025-02-28 17:22:47 -0300

    att 1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,24 +2,28 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        Carro carro = new Carro("Toyota", "Corolla", 2022, 5, "Gasolina", "Sedan");
-        Caminhao caminhao = new Caminhao("Volvo", "FH16", 2021, 2, "Diesel", 10);
-        Onibus onibus = new Onibus("Mercedes", "O500", 2020, 50, "Diesel", 6);
-        CarroEletrico carroEletrico = new CarroEletrico("Tesla", "Model S", 2023, 5, "Elétrico", "Sedan", 100);
-        CaminhaoRefrigerado caminhaoRefrigerado = new CaminhaoRefrigerado("Scania", "R450", 2022, 2, "Diesel", 15, -20);
 
+        Carro carro = new Carro("Toyota", "Supra MK4", 1998, 2, "Gasolina", "Esportivo");
         carro.exibirDetalhes();
         System.out.println("Autonomia: " + carro.calcularAutonomia() + " km\n");
 
+
+        Caminhao caminhao = new Caminhao("Mitsubishi", "Fuso Canter", 1995, 2, "Diesel", 5);
         caminhao.exibirDetalhes();
         System.out.println("Autonomia: " + caminhao.calcularAutonomia() + " km\n");
 
+
+        Onibus onibus = new Onibus("Hino", "Rainbow", 1990, 30, "Diesel", 6);
         onibus.exibirDetalhes();
         System.out.println("Autonomia: " + onibus.calcularAutonomia() + " km\n");
 
+
+        CarroEletrico carroEletrico = new CarroEletrico("Nissan", "Prairie EV", 1999, 4, "Elétrico", "Minivan", 40);
         carroEletrico.exibirDetalhes();
         System.out.println("Autonomia: " + carroEletrico.calcularAutonomia() + " km\n");
 
+
+        CaminhaoRefrigerado caminhaoRefrigerado = new CaminhaoRefrigerado("Isuzu", "Elf", 1997, 2, "Diesel", 3, -10);
         caminhaoRefrigerado.exibirDetalhes();
         System.out.println("Autonomia: " + caminhaoRefrigerado.calcularAutonomia() + " km\n");
     }
